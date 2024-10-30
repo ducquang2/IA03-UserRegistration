1fc4d32212d0e797ebbdb23e2604d1d12a6fca74
ducquang2 <nducquangg@gmail.com>
2024-10-30 15:21:17 +0700

    chore: update cors

--- ia03-be/src/main.ts
+++ ia03-be/src/main.ts
@@ -8,10 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: [
-      'https://ia-03-user-registration.vercel.app',
-      /\.ia-03-user-registration\.vercel\.app$/,
-    ],
+    origin: process.env.CLIENT_DOMAIN ?? 'http://localhost:5173/',
   });
 
   await app.listen(process.env.PORT ?? 3000);
